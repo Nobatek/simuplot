35d8830952dfa94298d79d3645fb3341e90bde18
Jérôme <jerome@Jessie.WORKSPACE>
2015-02-10 09:44:10 +0100

    Cosmetic changes in mlpwidget.py

--- mplwidget.py
+++ mplwidget.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Python Qt4 bindings for GUI objects
 from PyQt4 import QtGui
 
@@ -7,31 +9,34 @@ from PyQt4 import QtGui
 from matplotlib.backends.backend_qt4agg \
     import FigureCanvasQTAgg as FigureCanvas
 
+# Import de la toolbar associee a la figure
 from matplotlib.backends.backend_qt4agg \
-import NavigationToolbar2QT as NavigationToolbar #import de la toolbar associee a la figure
+    import NavigationToolbar2QT as NavigationToolbar 
 
 # Matplotlib Figure object
 import numpy as np
 from matplotlib.figure import Figure
 
+# https://github.com/matplotlib/matplotlib/blob/ebde05f5c1579c2ca28dab2a11ae5a674e4aac2f/examples/user_interfaces/embedding_in_qt4.py
 
 class MplCanvas(FigureCanvas):
     """Class to represent the FigureCanvas widget"""
     def __init__(self):
-        # setup Matplotlib Figure and Axis
+        
+        # Setup Matplotlib Figure and Axis
         self.fig = Figure(facecolor="white")
         self.axes = self.fig.add_subplot(111)
         
-        # initialization of the canvas
+        # Initialization of the canvas
         FigureCanvas.__init__(self, self.fig)
 
-        # we define the widget as expandable
-        FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
+        # Define the widget as expandable
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
 
-        # notify the system of updated policy
+        # Notify the system of updated policy
         FigureCanvas.updateGeometry(self)
-
-    
     
 class MplWidget(QtGui.QWidget):
     """Widget defined in Qt Designer"""
@@ -60,5 +65,3 @@ class MplWidget(QtGui.QWidget):
         self.canvas.axes.plot(x, y)
         self.canvas.draw()
 
-
-    
