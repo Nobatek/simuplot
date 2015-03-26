ecf1a6b6d2bd0e3097a1d956dbe7245b5994720b
Jérôme Lafréchoux <jerome@Jessie.Nobatek>
2015-03-26 18:20:54 +0100

    heatgainpie.py: Various code improvements

--- src/simuplot/dataplotter/heatgainpie.py
+++ src/simuplot/dataplotter/heatgainpie.py
@@ -18,29 +18,31 @@ from simuplot.data import DataTypes, DataZoneError
 # TODO: This module is broken if start date is not January 1st
 # The simulation length must be one year, and the period 'HOUR'
 # Should this be made more generic ?
-periods = [(translate('HeatGainPie', 'Year'),[[0,8760]]),
-           (translate('HeatGainPie', 'Summer'),[[2879,6553]]),
-           (translate('HeatGainPie', 'Winter'),[[0,2779],[6553,8760]]),
-           ]
+PERIODS = [
+    (translate('HeatGainPie', 'Year'), [[0, 8760]]),
+    (translate('HeatGainPie', 'Summer'), [[2879, 6553]]),
+    (translate('HeatGainPie', 'Winter'), [[0, 2779], [6553, 8760]]),
+    ]
 
 # TODO: put this stuff somewhere else
-heat_sources = ['HEATING_RATE',
-                'PEOPLE_HEATING_RATE',
-                'LIGHTING_HEATING_RATE',
-                'EQUIPMENT_HEATING_RATE',
-                'WINDOWS_HEATING_RATE',
-                'OPAQUE_SURFACES_HEATING_RATE',
-                'INFILTRATION_HEATING_RATE',
-                ]            
+HEAT_SOURCES = [
+    'HEATING_RATE',
+    'PEOPLE_HEATING_RATE',
+    'LIGHTING_HEATING_RATE',
+    'EQUIPMENT_HEATING_RATE',
+    'WINDOWS_HEATING_RATE',
+    'OPAQUE_SURFACES_HEATING_RATE',
+    'INFILTRATION_HEATING_RATE',
+    ]
 
 class HeatGainPie(DataPlotter):
 
     def __init__(self, building, color_chart):
-        
+
         super(HeatGainPie, self).__init__(building, color_chart)
 
         self._name = self.tr("Heat gains per source")
-        
+
         # Results dict
         self._heat_build_zone = None
 
@@ -50,28 +52,28 @@ class HeatGainPie(DataPlotter):
             [self.tr('Heat sources'), self.tr('Heat gains [kWh]')])
         self.dataTable.horizontalHeader().setResizeMode(
             QtGui.QHeaderView.ResizeToContents)
-            
+
         # Initialize table with one row per heat source with checkbox
-        self.dataTable.setRowCount(len(heat_sources))
-        for i, val in enumerate(heat_sources) :
+        self.dataTable.setRowCount(len(HEAT_SOURCES))
+        for i, val in enumerate(HEAT_SOURCES):
             # DataTypes is a dict of type:(unit, string)
             hs_name = QtCore.QCoreApplication.translate(
                 'Data', DataTypes[val][1])
             name_item = QtGui.QTableWidgetItem(hs_name)
             name_item.setFlags(QtCore.Qt.ItemIsUserCheckable |
-                           QtCore.Qt.ItemIsEnabled)
+                               QtCore.Qt.ItemIsEnabled)
             name_item.setCheckState(QtCore.Qt.Checked)
-            
+
             self.dataTable.setItem(i, 0, name_item)
-            
+
         # Set periodSelectBox
-        for per in periods:
+        for per in PERIODS:
             self.periodSelectBox.addItem(self.tr(per[0]))
 
         # Refresh data when periodSelectBox is activated
         self.periodSelectBox.activated.connect(
             self.refresh_data)
-            
+
         # Refresh plot when buildingSelectBox is modified
         self.buildingSelectBox.activated.connect(
             self.refresh_tab_and_plot)
@@ -79,15 +81,15 @@ class HeatGainPie(DataPlotter):
         # Refresh plot when zone is clicked/unclicked or sort order changed
         self.dataTable.itemClicked.connect(self.refresh_plot)
         self.dataTable.horizontalHeader().sectionClicked.connect(
-            self.refresh_plot)    
-            
+            self.refresh_plot)
+
     @property
     def name(self):
         return self._name
 
-    def ComputeHeatGains(self, zone, study_period):
+    def compute_heat_gains(self, zone, study_period):
         """Return heat gain for each source for the study period
-        
+
         Data is returned as a dict:
 
         {'HEATING_RATE':value,
@@ -95,10 +97,10 @@ class HeatGainPie(DataPlotter):
          ...
         }
         """
-        
+
         # Initialize full_year_vals list containing all values for each source
         full_year_vals = []
-        for hs in heat_sources:
+        for hs in HEAT_SOURCES:
             # If hourly heat source data not available, "mark it zero, Donnie"
             try:
                 vals = zone.get_values(hs, 'HOUR')
@@ -119,99 +121,101 @@ class HeatGainPie(DataPlotter):
         # Compute total gain [kWH] for each heat source
         # summing all intervals of the period
         # (Thanks to 2D array, all sources are processed at once)
-        gain_per_source = np.zeros(len(heat_sources))
+        gain_per_source = np.zeros(len(HEAT_SOURCES))
         for inter in study_period:
             # Extract values for interval
-            inter_val = full_year[:,inter[0]:inter[1]]
+            inter_val = full_year[:, inter[0]:inter[1]]
             # Sum gain for interval and add it to total
             gain_per_source += inter_val.sum(axis=1)
-        
+
         # Return results as a dictionary, with energy in [kWh]
-        return dict(zip(heat_sources, gain_per_source / 1000))
+        return dict(zip(HEAT_SOURCES, gain_per_source / 1000))
 
     @QtCore.pyqtSlot()
     def refresh_data(self):
-            
+
         # Get zones in building, sorted by name
         zones = self._building.zones
         zones.sort()
-        
-        # Set combobox with zone names 
+
+        # Set combobox with zone names
         # and add 'Building' as a ficticious "all zones" zone
         self.buildingSelectBox.addItems(zones)
         self.buildingSelectBox.addItem(self.tr('Building'))
-        self.buildingSelectBox.setCurrentIndex(self.buildingSelectBox.count() - 1)
-        
+        self.buildingSelectBox.setCurrentIndex(
+            self.buildingSelectBox.count() - 1)
+
         # Get the study period from combobox
-        study_period = periods[self.periodSelectBox.currentIndex()][1]
+        study_period = PERIODS[self.periodSelectBox.currentIndex()][1]
 
         # Compute heat gain per source in each zone
         self._heat_build_zone = {}
-        for name in zones :
+        for name in zones:
             # Compute heat gains for desired study period
             self._heat_build_zone[name] = \
-                self.ComputeHeatGains(self._building.get_zone(name),
-                                      study_period)
+                self.compute_heat_gains(self._building.get_zone(name),
+                                        study_period)
 
         # Compute heat gain per source for building by summing all zones
         self._heat_build_zone['Building'] = {}
-        for hs in heat_sources:
+        for hs in HEAT_SOURCES:
             self._heat_build_zone['Building'][hs] = \
                 sum([self._heat_build_zone[zone][hs] for zone in zones])
-        
+
         # Write in Table and draw plot
         self.refresh_tab_and_plot()
 
     @QtCore.pyqtSlot()
-    def refresh_tab_and_plot(self):    
+    def refresh_tab_and_plot(self):
 
         # Current zone or building displayed
-        if self.buildingSelectBox.currentIndex() == self.buildingSelectBox.count() - 1:
+        if (self.buildingSelectBox.currentIndex() ==
+            self.buildingSelectBox.count() - 1):
             cur_zone = 'Building'
-        else :
+        else:
             cur_zone = self.buildingSelectBox.currentText()
 
         # Display Zone or building value in table 2nd column
-        for i, hs in enumerate(heat_sources):
-        
+        for i, hs in enumerate(HEAT_SOURCES):
+
             # Get current heat gains for heat source in the zone
             hs_value = int(self._heat_build_zone[cur_zone][hs])
-            
+
             # Set item value for 2nd column
             val_item = QtGui.QTableWidgetItem()
             val_item.setData(QtCore.Qt.DisplayRole, hs_value)
             val_item.setFlags(QtCore.Qt.ItemIsEnabled)
             self.dataTable.setItem(i, 1, val_item)
-            
+
             # Uncheck heat source name if value is zero
-            name_item = self.dataTable.item(i,0)
+            name_item = self.dataTable.item(i, 0)
             if hs_value == 0:
                 name_item.setCheckState(QtCore.Qt.Unchecked)
-            else :
-                name_item.setCheckState(QtCore.Qt.Checked)      
-        
+            else:
+                name_item.setCheckState(QtCore.Qt.Checked)
+
         # Draw plot
         self.refresh_plot()
-        
+
     @QtCore.pyqtSlot()
     def refresh_plot(self):
-        
+
         canvas = self.plotWidget.canvas
-        
+
         # Clear axes
         canvas.axes.cla()
         canvas.set_tight_layout_on_resize(False)
-        
-        # Compute heat source sum and 
+
+        # Compute heat source sum and
         # create plot list removing unchecked values
         name_plot = []
         value_plot = []
         sum_value = 0
         for i in range(self.dataTable.rowCount()):
-            name = self.dataTable.item(i,0).text()
-            value = int(self.dataTable.item(i,1).text())
+            name = self.dataTable.item(i, 0).text()
+            value = int(self.dataTable.item(i, 1).text())
             sum_value += value
-            if self.dataTable.item(i,0).checkState() == QtCore.Qt.Checked:
+            if self.dataTable.item(i, 0).checkState() == QtCore.Qt.Checked:
                 name_plot.append(name)
                 value_plot.append(value)
 
@@ -228,8 +232,8 @@ class HeatGainPie(DataPlotter):
 
             # Set title
             title_str = self.tr('Heat gains repartition')
-            canvas.axes.set_title(title_str, y = 1.05)
-        
+            canvas.axes.set_title(title_str, y=1.05)
+
             canvas.set_tight_layout_on_resize(True)
 
         # Draw plot
