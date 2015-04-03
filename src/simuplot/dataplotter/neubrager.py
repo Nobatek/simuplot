2bf8e8bc338ec751e08320acd5b3bc847fa96cf5
unknown <bdurandestebe@eco170414.NOBATEK.local>
2015-04-03 18:23:39 +0200

    NEUBrager updated with plots functions

--- src/simuplot/dataplotter/neubrager.py
+++ src/simuplot/dataplotter/neubrager.py
@@ -25,6 +25,30 @@ from datetime import date, datetime, time, timedelta
 import matplotlib.dates as dates
 
 
+
+
+class lin_funct(object) :
+    """ define a linear function from its coefficient and
+        give a result for a * x + b
+        a and b must be floats object
+        x is an np.array
+    """   
+    def __init__(self, a, b) :
+        self._a = a
+        self._b = b
+    
+    def set_a(self,a) :
+        self._a = a
+     
+    def set_b(self,b) :
+        self._b = b
+
+    def compute(self, x) :  
+        b = np.ones(len(x)) * self._b
+        return self._a * x + self._b
+    
+
+
 def seven_day_mean(ext_temp, cur_day) :
     """Return a single temperature corresponding to the 
     mean temperature of the last 7 days according to EU norm
@@ -42,8 +66,6 @@ def seven_day_mean(ext_temp, cur_day) :
     seven_day_list = [[inter[0].replace(year = 2005),
                        inter[0].replace(year = 2005)]
                        for inter in seven_day_list]
-                       
-    print (seven_day_list)
     
     # Create a list of time interval objects
     seven_day_list = [TimeInterval(day) for day in seven_day_list]
@@ -52,15 +74,19 @@ def seven_day_mean(ext_temp, cur_day) :
     seven_temp = [ext_temp.mean_interval(inter)
                   for inter in seven_day_list]
     
-    print (seven_temp)
-    
-    print (sum(seven_temp)/len(seven_temp))
-    
     # Return seven_day_mean temperature
     return sum(seven_temp)/len(seven_temp)
     
              
     
+# Category definition from NF EN 15251
+category = {'I' : {'h_lim' : lin_funct(0.33, 18.8 + 2),
+                   'l_lim' : lin_funct(0.33, 18.8 - 2)},
+            'II' : {'h_lim' : lin_funct(0.33, 18.8 + 3),
+                    'l_lim' : lin_funct(0.33, 18.8 - 3)},
+            'III' : {'h_lim' : lin_funct(0.33, 18.8 + 4),
+                     'l_lim' : lin_funct(0.33, 18.8 - 4)}
+            }
 
 
 class NEUBrager(DataPlotter):
@@ -95,7 +121,11 @@ class NEUBrager(DataPlotter):
         
         # Refresh plot when BuildcomboBox is modified
         self.ComboZone.activated.connect(
-            self.refresh_plot)        
+            self.create_scatter)
+
+        # Refresh data when begin date or end date is changed
+        self.BeginDate.dateChanged.connect(self.create_scatter)
+        self.EndDate.dateChanged.connect(self.create_scatter)            
         
     @property
     def name(self):
@@ -119,7 +149,20 @@ class NEUBrager(DataPlotter):
         zone = self._building.get_zone(cur_zone)
         op_temp = zone.get_values('OPERATIVE_TEMPERATURE','HOUR')
         
-        seven_day_mean(ext_temp, summer_period.begin_date)
+        # Get the Operative Temperatures values for the period
+        y_op_temp = op_temp.get_interval(summer_period)
+        # print (y_op_temp)
+        
+        # Get a list of all days and hours in the summer period
+        day_list =  summer_period.get_datetimelist()
+        
+        # Create a list of mean ext temp for the period
+        x_mean_ext_temp = [seven_day_mean(ext_temp, dat.date())
+                           for dat in day_list]
+        # print (x_mean_ext_temp)
+        
+        return x_mean_ext_temp, y_op_temp
+        
 
         
         
@@ -159,13 +202,57 @@ class NEUBrager(DataPlotter):
         else:
             air_speed = None
             
-        # Compute scatter point coordinates
-        teta_mean, teta_op = self.ComputeScatter(cur_zone, summer_period)
-                                                    
-            
+        # Compute scatter point coordinates from zone and period
+        self._teta_mean, self._teta_op = self.ComputeScatter(cur_zone, summer_period)
+        
+        # Execute refresh_plot
+        self.refresh_plot()
         
     
+    def refresh_plot(self):
     
+        canvas = self._MplWidget.canvas
+        
+        # Clear axes
+        canvas.axes.cla()
+        canvas.set_tight_layout_on_resize(False)
+        
+        # Display points
+        canvas.axes.scatter(self._teta_mean, self._teta_op)
+        
+        # Create two x vector to plot the categories lines
+        x_high = np.array([10, 30])
+        x_low = np.array([15, 30])
+        
+        # Plot category upper lines
+        canvas.axes.plot(x_high, category['I']['h_lim'].compute(x_high),
+                         '--',
+                         color = "#8EC02F",
+                         linewidth = 1.5)
+        canvas.axes.plot(x_high, category['II']['h_lim'].compute(x_high),
+                         '--',
+                         color = "#E47823",
+                         linewidth = 1.5)
+        canvas.axes.plot(x_high, category['III']['h_lim'].compute(x_high),
+                         color = "r",
+                         linewidth = 1.5)
+
+                         
+        # Plot category lower lines
+        canvas.axes.plot(x_low, category['I']['l_lim'].compute(x_low),
+                         '--',
+                         color = "#8EC02F",
+                         linewidth = 1.5)
+        canvas.axes.plot(x_low, category['II']['l_lim'].compute(x_low),
+                         '--',
+                         color = "#E47823",
+                         linewidth = 1.5)
+        canvas.axes.plot(x_low, category['III']['l_lim'].compute(x_low),
+                         color = "r",
+                         linewidth = 1.5)  
+    
+        # Draw plot
+        canvas.draw()    
     
     
     
