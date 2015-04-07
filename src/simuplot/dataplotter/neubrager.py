26a9dc384923350c6a20e85270a1ba0883b1ee3e
unknown <bdurandestebe@eco170414.NOBATEK.local>
2015-04-07 17:24:59 +0200

    Fans, occupation and colours added to NEUBrager

--- src/simuplot/dataplotter/neubrager.py
+++ src/simuplot/dataplotter/neubrager.py
@@ -24,6 +24,8 @@ from datetime import date, datetime, time, timedelta
 
 import matplotlib.dates as dates
 
+from copy import deepcopy
+
 
 
 
@@ -31,11 +33,20 @@ class lin_funct(object) :
     """ define a linear function from its coefficient and
         give a result for a * x + b
         a and b must be floats object
-        x is an np.array
+        x is can be an int, a float, a list, or an np.array
+        as itss automatically convert in the compute method
     """   
     def __init__(self, a, b) :
         self._a = a
         self._b = b
+        
+    @property    
+    def b(self) :
+        return self._b
+    
+    @property
+    def a(self) :
+        return self._a
     
     def set_a(self,a) :
         self._a = a
@@ -43,10 +54,17 @@ class lin_funct(object) :
     def set_b(self,b) :
         self._b = b
 
-    def compute(self, x) :  
+    def compute(self, x) :
+        if x.__class__.__name__ == "int" or \
+           x.__class__.__name__ == "float" or \
+           x.__class__.__name__ == "float64" :
+            x = np.array([x])
+
+        elif x.__class__.__name__ == "list" :
+            x = np.array(x)
+
         b = np.ones(len(x)) * self._b
         return self._a * x + self._b
-    
 
 
 def seven_day_mean(ext_temp, cur_day) :
@@ -125,13 +143,22 @@ class NEUBrager(DataPlotter):
 
         # Refresh data when begin date or end date is changed
         self.BeginDate.dateChanged.connect(self.create_scatter)
-        self.EndDate.dateChanged.connect(self.create_scatter)            
+        self.EndDate.dateChanged.connect(self.create_scatter)
+
+        # Refresh data when Occupation checkbox is modified
+        self.OccCheckBox.stateChanged.connect(self.create_scatter)
+        
+        # Refresh plot when fan spinbox is modified
+        self.AirSpeedBox.valueChanged.connect(self.refresh_plot)
+        
+        # Refresh plot when fans are activated/deactivated
+        self.CheckFan.stateChanged.connect(self.refresh_plot)
         
     @property
     def name(self):
         return self._name
 
-    def ComputeScatter(self, cur_zone, summer_period):
+    def ComputeScatter(self, cur_zone, summer_period, zone_occ):
         """return Tmean Top coordinate and category colours
            for every hour for the current zone
         
@@ -149,17 +176,28 @@ class NEUBrager(DataPlotter):
         zone = self._building.get_zone(cur_zone)
         op_temp = zone.get_values('OPERATIVE_TEMPERATURE','HOUR')
         
-        # Get the Operative Temperatures values for the period
+        # Check if zone occupation is activated. If yes get people count
+        if zone_occ == True :
+            people_count = zone.get_values('PEOPLE_COUNT','HOUR')
+            people_count = people_count.get_interval(summer_period)
+                        
+        # Get the Operative Temperatures values for the whole summer period
         y_op_temp = op_temp.get_interval(summer_period)
-        # print (y_op_temp)
         
         # Get a list of all days and hours in the summer period
         day_list =  summer_period.get_datetimelist()
         
+        # if necessary Delete operative temperature and datetime
+        # from their np.array
+        if zone_occ == True :
+            y_op_temp = [y_op_temp[i] for i in range(len(people_count))
+                         if people_count[i] > 0]
+            day_list = [day_list[i] for i in range(len(people_count))
+                         if people_count[i] > 0]
+        
         # Create a list of mean ext temp for the period
         x_mean_ext_temp = [seven_day_mean(ext_temp, dat.date())
                            for dat in day_list]
-        # print (x_mean_ext_temp)
         
         return x_mean_ext_temp, y_op_temp
         
@@ -181,17 +219,24 @@ class NEUBrager(DataPlotter):
         self.create_scatter()
         
     def create_scatter(self):
-        # Convert Qdatetime object BeinDate into date object
+        # Convert Qdatetime object BeginDate into date object
         begin_date = self.BeginDate.date()
         year, month, day = begin_date.getDate()
         begin_date = date(year, month, day)
         
-        # Convert Qdatetime object end_date into date object
+        # Convert Qdatetime object EndDate into date object
         end_date = self.EndDate.date()
         year, month, day = end_date.getDate()
         end_date = date(year, month, day)
         
+        # Create the summer TimeInterval object
         summer_period = TimeInterval([begin_date, end_date])
+
+        # Check if occupation is taken into account
+        if self.OccCheckBox.checkState() == QtCore.Qt.Checked :
+            zone_occ = True
+        else :
+            zone_occ = None
         
         # Get zone to plot
         cur_zone = self.ComboZone.currentText()
@@ -203,7 +248,9 @@ class NEUBrager(DataPlotter):
             air_speed = None
             
         # Compute scatter point coordinates from zone and period
-        self._teta_mean, self._teta_op = self.ComputeScatter(cur_zone, summer_period)
+        self._teta_mean, self._teta_op = self.ComputeScatter(cur_zone,
+                                                             summer_period,
+                                                             zone_occ)
         
         # Execute refresh_plot
         self.refresh_plot()
@@ -217,38 +264,79 @@ class NEUBrager(DataPlotter):
         canvas.axes.cla()
         canvas.set_tight_layout_on_resize(False)
         
-        # Display points
-        canvas.axes.scatter(self._teta_mean, self._teta_op)
-        
         # Create two x vector to plot the categories lines
-        x_high = np.array([10, 30])
-        x_low = np.array([15, 30])
+        x_high = [10, 30]
+        x_low = [15, 30]
+        
+        # Assign colour for the three category and for the over temperature
+        I_colour = "#8EC02F"
+        II_colour = "#E47823"
+        III_colour = "#6A4300"
+        ot_colour = "#868786"
+        
+        # If fans are present in zone, get the air speed value
+        # Get the categories
+        used_category = deepcopy(category)
+        if self.CheckFan.checkState() == QtCore.Qt.Checked :
+            # Get the air speed
+            a_s = self.AirSpeedBox.value()
+            
+            # Compute the limit temperature rise (NF EN 15251)
+            # The 4th order polynome is obtained by regression from
+            # a digitized plot extract from the norm
+            cor = -3.1365*a_s**4 + 12.55*a_s**3 - 18.905*a_s**2 + 14.551*a_s -2.088 
+            
+            # Apply correction to used_category
+            for cat in used_category.keys() :
+                cat_b = category[cat]['h_lim'].b
+                used_category[cat]['h_lim'].set_b(cat_b + cor)
+            
+        # Determine in which category each point falls
+        # create a colour list for the scatter plot
+        colours = [ot_colour 
+                   if t_op > used_category['III']['h_lim'].compute(t_mean) else 
+                   III_colour 
+                   if t_op > used_category['II']['h_lim'].compute(t_mean) else
+                   II_colour 
+                   if t_op > used_category['I']['h_lim'].compute(t_mean) else
+                   I_colour 
+                   if t_op > used_category['I']['l_lim'].compute(t_mean) else
+                   II_colour 
+                   if t_op > used_category['II']['l_lim'].compute(t_mean) else
+                   III_colour 
+                   if t_op > used_category['III']['l_lim'].compute(t_mean) else
+                   ot_colour for t_mean, t_op in zip(self._teta_mean, self._teta_op)]
+        
+        # Display points
+        canvas.axes.scatter(self._teta_mean, self._teta_op,
+                            c = colours,
+                            edgecolor = colours)
         
         # Plot category upper lines
-        canvas.axes.plot(x_high, category['I']['h_lim'].compute(x_high),
+        canvas.axes.plot(x_high, used_category['I']['h_lim'].compute(x_high),
                          '--',
-                         color = "#8EC02F",
+                         color = I_colour,
                          linewidth = 1.5)
-        canvas.axes.plot(x_high, category['II']['h_lim'].compute(x_high),
+        canvas.axes.plot(x_high, used_category['II']['h_lim'].compute(x_high),
                          '--',
-                         color = "#E47823",
+                         color = II_colour,
                          linewidth = 1.5)
-        canvas.axes.plot(x_high, category['III']['h_lim'].compute(x_high),
-                         color = "r",
+        canvas.axes.plot(x_high, used_category['III']['h_lim'].compute(x_high),
+                         color = III_colour,
                          linewidth = 1.5)
 
                          
         # Plot category lower lines
-        canvas.axes.plot(x_low, category['I']['l_lim'].compute(x_low),
+        canvas.axes.plot(x_low, used_category['I']['l_lim'].compute(x_low),
                          '--',
-                         color = "#8EC02F",
+                         color = I_colour,
                          linewidth = 1.5)
-        canvas.axes.plot(x_low, category['II']['l_lim'].compute(x_low),
+        canvas.axes.plot(x_low, used_category['II']['l_lim'].compute(x_low),
                          '--',
-                         color = "#E47823",
+                         color = II_colour,
                          linewidth = 1.5)
-        canvas.axes.plot(x_low, category['III']['l_lim'].compute(x_low),
-                         color = "r",
+        canvas.axes.plot(x_low, used_category['III']['l_lim'].compute(x_low),
+                         color = III_colour,
                          linewidth = 1.5)  
     
         # Draw plot
